285c86036bb6ae7ae83851cdff7da24c5b0e35a5
Talha Khalid <dataanalyst714@gmail.com>
2025-03-24 13:54:01 +0500

    Update application.py

--- application.py
+++ application.py
@@ -12,6 +12,8 @@ from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 from sklearn.preprocessing import LabelEncoder
 from packaging.version import Version as LooseVersion
+pip install yellowbrick==1.5
+
 
 
 # Load Dataset
